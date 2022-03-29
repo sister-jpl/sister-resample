8193e4c661fd4fdc604d69dc646dc24ca7034aa2
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-03-29 19:23:38 -0400

    Added spectral resampler

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -3,16 +3,16 @@ pge_dir=$(dirname ${imgspec_dir})
 echo $pge_dir
 mkdir output
 
-string='location'
-
 for a in `python get_paths_from_granules.py`; 
-	do 
-	  if [[ $a == *"loc"* ]]; then
-	    echo 'Orthocorrecting loc file'
-	    python ${imgspec_dir}/create_loc_ort.py $a
-	    python ${pge_dir}/spatial_resample.py "${a}_ort" output --verbose;
-	  else
-	   python ${pge_dir}/spatial_resample.py $a output --verbose;
-	  fi
-	done
+   do 
+      if [[ $a == *"loc"* ]]; then
+         echo 'Orthocorrecting loc file'
+         python ${imgspec_dir}/create_loc_ort.py $a
+         python ${pge_dir}/spatial_resample.py "${a}_ort" output --verbose;
+      elif [[ $a == *"rfl"* ]]; then
+         python ${pge_dir}/spectral_resample.py $a output --verbose;
+      else
+         python ${pge_dir}/spatial_resample.py $a output --verbose;
+      fi
+done
 
