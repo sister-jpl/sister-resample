7d5b0d9174576f0c240cfd91b3eaa010800ee483
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-03-30 13:49:31 -0400

    Added compression

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -2,17 +2,25 @@ imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
 pge_dir=$(dirname ${imgspec_dir})
 
 mkdir output
+tar_file=$(ls input/*tar.gz)
+
+base=$(basename $tar_file)
+output_dir=output/${base%.tar.gz}
+
+tar -xzvf $tar_file -C input
 
 for a in `python get_paths_from_granules.py`; 
    do 
       if [[ $a == *"loc"* ]]; then
          echo 'Orthocorrecting loc file'
          python ${imgspec_dir}/create_loc_ort.py $a
-         python ${pge_dir}/spatial_resample.py "${a}_ort" output --verbose;
+         python ${pge_dir}/spatial_resample.py "${a}_ort" $output_dir --verbose;
       elif [[ $a == *"rfl"* ]]; then
-         python ${pge_dir}/spectral_resample.py $a output --verbose;
+         python ${pge_dir}/spectral_resample.py $a $output_dir --verbose;
       else
-         python ${pge_dir}/spatial_resample.py $a output --verbose;
+         python ${pge_dir}/spatial_resample.py $a $output_dir --verbose;
       fi
   done
 
+tar -czvf $output_dir.tar.gz $output_dir
+rm -r $output_dir
