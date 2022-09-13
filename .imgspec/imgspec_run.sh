f82c0ef6d4c285876ae86d939fb0c1f84978c379
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-09-13 08:51:59 -0700

    Updated filenaming

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -9,23 +9,31 @@ pge_dir=$(dirname ${imgspec_dir})
 
 mkdir output
 
-tar -xzvf input/*.tar.gz -C input
+#tar -xzvf input/*.tar.gz -C input
 
-rfl_path=$(ls input/*/*RFL* | grep -v '.hdr')
+rfl_path=$(ls input/*/*RFL* | grep -v '.hdr\|.log')
 unc_path=$(ls input/*/*UNC* | grep -v '.hdr')
+
+echo "Found input RFL file: $rfl_path"
+echo "Found input UNC file: $unc_path"
 
 # Create output directory
 rfl_name=$(basename $rfl_path)
-output_base_name=$(echo "${rdn_name/L2A_RFL/"L2A_RSRFL"}")
+output_base_name=$(echo "${rfl_name/L2A_RFL/"L2A_RSRFL"}")
 mkdir output/$output_base_name
 
 # Resample uncertainty and reflectance
 python ${pge_dir}/spectral_resample.py $rfl_path output/$output_base_name
 python ${pge_dir}/spectral_resample.py $unc_path output/$output_base_name
 
-
 #Rename, compress and cleanup outputs
-
 cd output
+
+mv */*_RFL*.hdr $output_base_name/$output_base_name.hdr
+mv */*_RFL* $output_base_name/$output_base_name
+
+mv */*_UNC*.hdr $output_base_name/$(echo "${output_base_name/RSRFL/"RSUNC"}").hdr
+mv */*_UNC* $output_base_name/$(echo "${output_base_name/RSRFL/"RSUNC"}")
+
 tar -czvf ${output_base_name}.tar.gz $output_base_name
 rm -r $output_base_name
