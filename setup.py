2daa88d2488a1710cb5b61fe254c54a5c04d5ec4
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-10-10 12:42:38 -0700

    Updated version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with Path(__file__).parent.joinpath('requirements.txt').open() as f:
 
 setup(
     name='sister-resample',
-    version='1.0',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
