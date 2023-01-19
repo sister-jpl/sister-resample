f54ef29f713d23b9c888552b25589670d28ee1fb
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-01-19 08:37:08 -0800

    Added new scripts

---
+++ pge_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source activate sister
+
+pge_dir=$(cd "$(dirname "$0")" ; pwd -P)
+
+cat inputs.json
+
+echo "Creating runconfig"
+python ${pge_dir}/generate_runconfig.py inputs.json
+
+echo "Running L1 Preprocess PGE"
+python ${pge_dir}/spectral_resample.py runconfig.json
